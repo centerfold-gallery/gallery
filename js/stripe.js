8295708840704e79b5ee3fabf8d8b7a13bad1520
Mackenzie Davidson <mwvdavidson@gmail.com>
2017-05-24 19:40:11 -0400

    Payments
    removed: custom stripe items
    added: simple buy button
    added: heroku server stuff
    added: success and error pages

--- js/stripe.js
+++ js/stripe.js
@@ -2,6 +2,8 @@ var handler = StripeCheckout.configure({
       key: 'pk_test_C387opaZCGYru4uaKfthZATA',
       image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
       locale: 'auto',
+      billingAddress: 'true',
+      zipCode: 'true',
       token: function(token) {
         // You can access the token ID with `token.id`.
         // Get the token ID to your server-side code for use.
@@ -12,9 +14,9 @@ var handler = StripeCheckout.configure({
       // Open Checkout with further options:
       handler.open({
         name: 'CENTERFOLD',
-        description: '2 widgets',
+        description: 'Galaxy SOHO',
         currency: 'cad',
-        amount: 2000
+        amount: 30000
       });
       e.preventDefault();
     });
