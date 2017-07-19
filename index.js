650be88b1a97409c8c5f20405e3816c949d45979
mweiss17 <martin.weiss@mail.mcgill.ca>
2017-07-19 17:19:48 -0400

    added static asset routes to the node servers index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+var path = require('path')
+
+app.use('/js', express.static('js'))
+app.use('/stylesheets', express.static('stylesheets'))
+app.use('/assets', express.static('assets'))
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(3000, function () {
