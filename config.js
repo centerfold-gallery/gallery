4daf564c535966f2b3aa4ccce2db12bdb28589b9
mweiss17 <martin.weiss@mail.mcgill.ca>
2017-07-25 13:26:33 -0400

    added heroku config vars reader

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
-// Parse the config.json file
+// if running on heroku, get heroku config variables
+if(process.env){
+    var parsed = {"airtableAPIKey": process.env.airtableAPIKey,
+                  "airtableBase": process.env.airtableBase}
+}
 
-var fs = require('fs'),
-configPath = './config.json';
-var parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
-exports.storageConfig = parsed;
+// If local development, parse the config.json file
+else{
+    var fs = require('fs'),
+    configPath = './config.json';
+    var parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
+    exports.storageConfig = parsed;
+}
