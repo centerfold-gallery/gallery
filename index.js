1b335c5c786962ecdf13093d43f6fc84bc619741
mweiss17 <martin.weiss@mail.mcgill.ca>
2017-07-27 14:26:59 -0400

    added queries to pull art and artist information for home and art pages

--- index.js
+++ index.js
@@ -22,17 +22,109 @@ app.use('/assets', express.static('assets'))
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 
+app.get('/', function(req, res){
+    
+    var context = {};
 
-// Page Routes
-app.get('/', function (req, res) {
-  res.render('index.html', {});
-});
-app.get('/#', function (req, res) {
-  res.render('index.html', {});
+    var getCollection = function(title) {
+        return new Promise(function(resolve, reject) {
+            base('Online Featured Collection')
+                .select({view: "Grid view", filterByFormula: "{Title} = '"+ title +"'"})
+                .firstPage(function(err, homepageArt) {
+                    if(err) { 
+                        reject(err) 
+                    }
+                    else {
+                        var jsonHomepageArt = homepageArt.map(function(homepageArt){
+                            return homepageArt['_rawJson']
+                        });
+                        resolve(jsonHomepageArt[0]['fields']['Artworks']);                    
+                    }
+                });
+            });
+        }
+
+    var getArtwork = function(filterStatement){
+        return new Promise(function(resolve, reject) {
+        base('Artworks')
+            .select({view: "Grid view", filterByFormula: filterStatement})
+            .firstPage(function(err, homepageArtworks) {
+                if(err) { 
+                    reject(err) 
+                }
+                else {
+                    var jsonHomepageArtworks = homepageArtworks.map(function(homepageArtworks){
+                        return homepageArtworks['_rawJson']
+                    });
+                    resolve(jsonHomepageArtworks);                    
+                }
+            });
+        });
+    }
+
+    var getArtists = function(filterStatement){
+        return new Promise(function(resolve, reject) {
+        base('Artists')
+            .select({view: "Grid view", filterByFormula: filterStatement})
+            .firstPage(function(err, artists) {
+                if(err) { 
+                    reject(err) 
+                }
+                else {
+                    var jsonArtists = artists.map(function(artist){
+                        return artist['_rawJson']
+                    });
+                    resolve(jsonArtists);                    
+                }
+            });
+        });
+    }
+
+    function constructFilterStatement(records){
+        var filterStatement = "OR("
+        for (var i = 0; i < records.length; i++) {
+            filterStatement = filterStatement + "RECORD_ID() = '" + records[i] + "'";
+            if (i < records.length - 1){
+                filterStatement = filterStatement + ',';
+            }
+        }
+        filterStatement = filterStatement + ")";
+        return filterStatement;
+    }
+
+    getCollection('Homepage').then(function(result) {
+        var filterStatement = constructFilterStatement(result);
+        getArtwork(filterStatement).then(function(result){
+            context['homepageArtworks'] = result;
+            getCollection('Art Page').then(function(result){
+                var filterStatement = constructFilterStatement(result);
+                getArtwork(filterStatement).then(function(result){
+                    context['artPageArtworks'] = result;
+                    // Parse the list of all the artist's ids
+                    var artistIDs = [];
+                    for (var i = 0; i < result.length; i++) {
+                        artistIDs[i] = result[i].fields.Artist[0];
+                    }
+                    var artPageArtistsFilterStatement = constructFilterStatement(artistIDs)
+                    getArtists(artPageArtistsFilterStatement).then(function(result){
+                        for(var i = 0; i < context['artPageArtworks'].length; i++){
+                            for(var j = 0; j < result.length; j++){
+                                if(result[j].id == context['artPageArtworks'][i].fields.Artist[0]){
+                                    context['artPageArtworks'][i].fields.Artist = result[j].fields['Full Name'];
+                                }
+                            }
+                        }
+                        console.log(context['artPageArtworks'])
+                        res.render('index.html', context);
+                    });
+                });
+            });
+        });
+    });
 });
 
 app.get('/events', function (req, res) {
-    context = {title: "Events"};
+    var context = {title: "Events"};
 
     var getUpcomingEvents = new Promise(function(resolve, reject) {
         base('Events')
