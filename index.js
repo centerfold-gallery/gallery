f528e1984ed2d8fea52726bb12e5923fc4e48d95
Mackenzie Davidson <mwvdavidson@gmail.com>
2017-10-25 20:22:49 -0400

    big ABORT

--- index.js
+++ index.js
@@ -400,8 +400,8 @@ app.get('/error', function (req, res) {
   res.render('error/index.html', {title: "About", static_url: "https://s3.amazonaws.com/centerfold-website/"});
 });
 
-app.get('/art/buy-button/index.html', function (req, res) {
-  res.render('art/buy-button/index.html', {title: "Buy", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
+app.get('/susan-nelson', function (req, res) {
+  res.render('susan-nelson/index.html', {title: "Susan Nelson", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
 });
 
 
