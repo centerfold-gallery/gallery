2d5df909739b100150a7700c68d16cd1de074985
Mackenzie Davidson <mwvdavidson@gmail.com>
2017-11-08 19:33:32 -0500

    added sub-views

--- routes/buy-pages.js
+++ routes/buy-pages.js
@@ -1,7 +1,31 @@
 module.exports = function(app) {
 
     app.get('/art/CF000001', function (req, res) {
-      res.render('art/buy/CF000001.html', {title: "Art 1", static_url: "https://s3.amazonaws.com/centerfold-website/"});
+
+        // base('Artworks')
+        //     .find('recnGllYR7ZAwIYs8', function(err, record) {
+        //     if(err) {
+        //         console.error(err); return;
+        //     }
+        //     else {
+        //         var jsonHomepageArt = homepageArt.map(function(homepageArt){
+        //             return homepageArt['_rawJson']
+        //         });
+        //         resolve(jsonHomepageArt[0]['fields']['Artworks']);
+        //     }
+        // });
+
+        var Airtable = require('airtable');
+        var base = new Airtable({apiKey: 'keyy1OWD9fbKh3xIL'}).base('appAxg6rhUJ9BZmV4');
+
+        base('Artworks').find('recnGllYR7ZAwIYs8', function(err, record) {
+            if (err) {
+                console.error(err); return;
+            }
+            console.log(record);
+        });
+
+        res.render('art/buy/CF000001.html', {title: "Art 1", static_url: "https://s3.amazonaws.com/centerfold-website/"});
     });
 
 }
