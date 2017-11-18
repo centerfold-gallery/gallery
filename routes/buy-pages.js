4315d64c2285bec0e50eb25cab6fb79fcdcebfd1
Mackenzie Davidson <mwvdavidson@gmail.com>
2017-11-17 19:45:32 -0500

    buy pages work now (need a bit of configuration w context)

--- routes/buy-pages.js
+++ routes/buy-pages.js
@@ -22,10 +22,49 @@ module.exports = function(app) {
             if (err) {
                 console.error(err); return;
             }
+            // res.json(record);
             console.log(record);
+
+            res.render(
+                'art/buy/CF000001.html',
+                record
+            );
         });
 
-        res.render('art/buy/CF000001.html', {title: "Art 1", static_url: "https://s3.amazonaws.com/centerfold-website/"});
+        // var object_to_render = {key: "value", ...};
+        //
+        // fs.readFile(path_to_mustache_template, function (err, data) {
+        //     if (err) throw err;
+        //     var output = Mustache.render(data.toString(), object_to_render);
+        //
+        //   // do something with output...
+        // });
+
+
+        // var getOneArtwork = new Promise(function(resolve, reject) {
+        //     base('Artworks')
+        //         .find('recnGllYR7ZAwIYs8', function(err, OneArtworks) {
+        //             if(err) {
+        //                 reject(err)
+        //             }
+        //             else {
+        //                 var jsonOneArtworks = OneArtworks.map(function(OneArtwork){
+        //                     return OneArtwork['fields']
+        //                 });
+        //                 resolve(jsonOneArtworks);
+        //                 console.log(record);
+        //             }
+        //     });
+        // });
+        //
+        // getOneArtwork.then(function (result){
+        //     context['OneArtworks'] = result;
+        //     res.render(
+        //         'art/buy/CF000001.html',
+        //         {title: "Art 1", static_url: "https://s3.amazonaws.com/centerfold-website/"},
+        //         context
+        //     );
+        // });
     });
 
 }
