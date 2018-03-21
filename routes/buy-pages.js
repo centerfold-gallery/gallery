bb999d080b90546dcd65f505fb4668bcf088a806
Nadim El-Asmar <nadim.as25@gmail.com>
2018-03-21 15:03:31 -0400

    new artwork

--- routes/buy-pages.js
+++ routes/buy-pages.js
@@ -10433,7 +10433,25 @@ module.exports = function(app) {
                     );
                 });
             });
+        app.get('/art/CF000589', function (req, res) {
 
+                      var Airtable = require('airtable');
+                      var base = new Airtable({apiKey: config.storageConfig.airtableAPIKey}).base(config.storageConfig.airtableBase);
+                      var context = {static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey};
+
+                      base('Artworks').find('reci4ishRROQCrGsG', function(err, record) {
+                          if (err) {
+                              console.error(err); return;
+                          }
+                          //res.json(record);
+                          console.log(record);
+
+                          res.render(
+                              'art/buy/buy-page.html',
+                              record
+                          );
+                      });
+                  });
 
 
 
