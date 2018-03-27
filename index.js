1987da7d95795088b8a077004339ff167fce0468
Mackenzie Davidson <mwvdavidson@gmail.com>
2018-03-27 16:51:52 -0400

    changed model to series (expect something breaking here)

--- index.js
+++ index.js
@@ -1991,107 +1991,11 @@ app.get('/art/20', function(req, res){
 
 // CF MODEL
 
-// app.get('/model/test', function(req, res){
-//     var context = {title: "Test", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey};
-//
-//     var getCollection = function(title) {
-//         return new Promise(function(resolve, reject) {
-//             base('Model')
-//                 .select({view: "Grid view", filterByFormula: "{Title} = '"+ title +"'"})
-//                 .firstPage(function(err, homepageArt) {
-//                     if(err) {
-//                         reject(err)
-//                     }
-//                     else {
-//                         var jsonHomepageArt = homepageArt.map(function(homepageArt){
-//                             return homepageArt['_rawJson']
-//                         });
-//                         resolve(jsonHomepageArt[0]['fields']['Artworks']);
-//                     }
-//                 });
-//             });
-//         }
-//
-//     var getArtwork = function(filterStatement){
-//         return new Promise(function(resolve, reject) {
-//         base('Artworks')
-//             .select({view: "Grid view", filterByFormula: filterStatement})
-//             .firstPage(function(err, homepageArtworks) {
-//                 if(err) {
-//                     reject(err)
-//                 }
-//                 else {
-//                     var jsonHomepageArtworks = homepageArtworks.map(function(homepageArtworks){
-//                         return homepageArtworks['_rawJson']
-//                     });
-//                     resolve(jsonHomepageArtworks);
-//                 }
-//             });
-//         });
-//     }
-//
-//     var getArtists = function(filterStatement){
-//         return new Promise(function(resolve, reject) {
-//         base('Artists')
-//             .select({view: "Grid view", filterByFormula: filterStatement})
-//             .firstPage(function(err, artists) {
-//                 if(err) {
-//                     reject(err)
-//                 }
-//                 else {
-//                     var jsonArtists = artists.map(function(artist){
-//                         return artist['_rawJson']
-//                     });
-//                     resolve(jsonArtists);
-//                 }
-//             });
-//         });
-//     }
-//
-//     function constructFilterStatement(records){
-//         var filterStatement = "OR("
-//         for (var i = 0; i < records.length; i++) {
-//             filterStatement = filterStatement + "RECORD_ID() = '" + records[i] + "'";
-//             if (i < records.length - 1){
-//                 filterStatement = filterStatement + ',';
-//             }
-//         }
-//         filterStatement = filterStatement + ")";
-//         return filterStatement;
-//     }
-//
-//     getCollection('Test').then(function(result) {
-//         var filterStatement = constructFilterStatement(result);
-//         getArtwork(filterStatement).then(function(result){
-//             context['homepageArtworks'] = result;
-//             getCollection('Test').then(function(result){
-//                 var filterStatement = constructFilterStatement(result);
-//                 getArtwork(filterStatement).then(function(result){
-//                     context['artPageArtworks'] = result;
-//                     // Parse the list of all the artist's ids
-//                     var artistIDs = [];
-//                     for (var i = 0; i < result.length; i++) {
-//                         artistIDs[i] = result[i].fields.Artist[0];
-//                     }
-//                     var artPageArtistsFilterStatement = constructFilterStatement(artistIDs)
-//                     getArtists(artPageArtistsFilterStatement).then(function(result){
-//                         for(var i = 0; i < context['artPageArtworks'].length; i++){
-//                             for(var j = 0; j < result.length; j++){
-//                                 if(result[j].id == context['artPageArtworks'][i].fields.Artist[0]){
-//                                     context['artPageArtworks'][i].fields.Artist = result[j].fields['Full Name'];
-//                                 }
-//                             }
-//                         }
-//                         res.render('model/collection/MC000001.html', context);
-//
-//                     });
-//                 });
-//             });
-//         });
-//     });
-// });
+app.get('/series', function (req, res) {
+  res.render('series/index.html', {title: "Series", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
+});
 
-app.get('/model/test', function(req, res){
+app.get('/series/test', function(req, res){
         var Airtable = require('airtable');
         var base = new Airtable({apiKey: config.storageConfig.airtableAPIKey}).base(config.storageConfig.airtableBase);
         var context = {title: "Test", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey};
@@ -2191,7 +2095,7 @@ app.get('/model/test', function(req, res){
                             });
                             var ar;
                             console.log(context);
-                            res.render('model/collection/MC000001.html', context, ar)
+                            res.render('series/collection/SC000001.html', context, ar)
                         });
                     });
                 });
