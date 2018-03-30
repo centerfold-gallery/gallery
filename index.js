d8e0a72a108332cc1e015b8732e1a4022fd31780
Mackenzie Davidson <mwvdavidson@gmail.com>
2018-03-30 17:17:03 -0400

    faqs broken up, wild CAD removed

--- index.js
+++ index.js
@@ -2526,6 +2526,10 @@ app.get('/about', function (req, res) {
   res.render('about/index.html', {title: "About", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
 });
 
+app.get('/faqs', function (req, res) {
+  res.render('faqs/index.html', {title: "FAQs", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
+});
+
 app.get('/faqs/marketplace', function (req, res) {
   res.render('faqs/marketplace.html', {title: "FAQs", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
 });
