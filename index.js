d8b2245af7c481f51a7c27f9713c24db5be64e23
Mackenzie Davidson <mwvdavidson@gmail.com>
2018-03-30 17:47:57 -0400

    chill master commit to prepare for staging FC000003

--- index.js
+++ index.js
@@ -2530,14 +2530,6 @@ app.get('/faqs', function (req, res) {
   res.render('faqs/index.html', {title: "FAQs", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
 });
 
-app.get('/faqs/marketplace', function (req, res) {
-  res.render('faqs/marketplace.html', {title: "FAQs", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
-});
-
-app.get('/faqs/series', function (req, res) {
-  res.render('faqs/series.html', {title: "FAQs", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
-});
-
 app.get('/privacy', function (req, res) {
   res.render('privacy/index.html', {title: "Privacy Policy", static_url: "https://s3.amazonaws.com/centerfold-website/", stripeAPIKey: config.storageConfig.stripeAPIKey});
 });
